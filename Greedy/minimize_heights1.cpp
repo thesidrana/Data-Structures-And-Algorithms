a099233f50444183e54d4bc733dbf3858418b286
Siddharth Rana <56639199+thesidrana@users.noreply.github.com>
2021-08-16 14:37:12 +0530

    Create minimize_heights1.cpp

---
+++ Greedy/minimize_heights1.cpp
@@ -0,0 +1,20 @@
+// Given an array arr[] denoting heights of N towers and a positive integer K, you have to 
+// modify the height of each tower either by increasing or decreasing them by K only once.
+// Find out what could be the possible minimum difference of the height of shortest and longest 
+// towers after you have modified each tower.
+
+class Solution{   
+public:
+    int getMinDiff(int arr[], int n, int k) {
+        sort(arr, arr+n);
+        int minEle, maxEle;
+        int result = arr[n-1] - arr[0];
+        for(int i = 1; i <= n -1; i++){
+            maxEle = max(arr[i-1]+k, arr[n-1]-k);
+            minEle = min(arr[0]+k, arr[i]-k);
+            result = min(result, maxEle - minEle);
+        }
+        return result;
+    }
+    
+};
